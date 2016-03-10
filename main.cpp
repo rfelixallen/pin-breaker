bf0170fb767cda8d45f7e9cd23955ec2f24158a1
Robert Allen <rfelixallen@gmail.com>
2016-03-10 07:50:32 -0500

    compared ints

--- main.cpp
+++ main.cpp
@@ -6,5 +6,15 @@ int main(int argc, char *argv[]) {
         std::cout << count << " " << argv[count] << '\n';
     std::cout << "The number you passed in is " << argv[1] << ".\n";
  
+    // Method 1: Try all numbers from 0000 to 9999
+    //for (int count=0; count < 10000; ++count)
+    int i1 = 0;
+    int z1 = 0;
+    int i2 = 0000;
+    int i3 = 05;
+
+    if (i1 == z1)
+    	std::cout << i1 << " is equal to " << z1 << ".\n";
+
     return 0;
 }
